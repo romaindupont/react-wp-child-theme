5825ec7081e7612b76458a4e4e2bd14251dc58ba
Romain DUPONT <RDUPONT@hydequip.lan>
2022-07-22 14:35:20 +0200

    recuperation headerphp

--- functions.php
+++ functions.php
@@ -15,3 +15,10 @@ function my_theme_enqueue_styles() {
 		'20220413'
 	); 
 }
+
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
+
+add_filter( 'show_admin_bar', '__return_false' );
