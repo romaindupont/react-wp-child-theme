9262812018dcb07bc99cd08f7f5e8d6f7cd856ed
Romain DUPONT <RDUPONT@hydequip.lan>
2022-07-27 11:12:11 +0200

    create body component

---
+++ src/components/body/index.js
@@ -0,0 +1,7 @@
+const Body = () => {
+	return (
+		<main class="configurator" id="configurator"></main>
+	)
+}
+
+export default Body;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const { render, useState } = wp.element;
 import './style.scss';
 import Header from './components/header';
+import Body from './components/body';
 
 const Veldt = () => {
   return (
-		<>
+		<div className='veldtConfig'>
 			<Header />
-		</>
+			<Body />
+		</div>
   );
 };
 render(<Veldt />, document.getElementById('root') || document.createElement('div'));
