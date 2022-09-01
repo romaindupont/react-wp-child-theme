9b5bc53921c48d13a63c8b25c525cf579195a5f1
Romain DUPONT <RDUPONT@hydequip.lan>
2022-09-01 16:19:32 +0200

    localid configuration

---
+++ assets/json/helmetid.js
@@ -0,0 +1,249 @@
+export default [
+	{
+		helmet:	{
+			helmetNoGroove:
+			{
+				parentLocalId: 1,
+				localId: 2,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmet_noGroove"
+			},
+			helmetGroove:
+			{
+				parentLocalId: 1,
+				localId: 3,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmet_groove"
+			},
+			helmetElements:
+			{
+				parentLocalId: 1,
+				localId: 4,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmet_elements"
+			},
+			helmetDesignGroove: 
+			{
+				parentLocalId: 1,
+				localId: 5,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetDesign_groove"
+			},
+			helmetDesignNoGroove: 
+			{
+				parentLocalId: 1,
+				localId: 6,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetDesign_noGroove"
+			},
+			helmetCoatingGroove:
+			{
+				parentLocalId: 1,
+				localId: 7,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetCoating_groove"
+			},
+			helmetCoatingNoGroove:
+			{
+				parentLocalId: 1,
+				localId: 8,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetCoating_noGroove"
+			},
+			helmetTrimRubber:
+			{
+				parentLocalId: 1,
+				localId: 9,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetTrim_rubber"
+			},
+			helmetTrimLeather:
+			{
+				parentLocalId: 1,
+				localId: 10,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "helmetTrim_leather"
+			}
+		},
+		flap: {
+			pullingFlapNylon:
+			{
+				parentLocalId: 1,
+				localId: 11,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "pullingFlap_nylon"
+			},
+			pullingFlapLeather:
+			{
+				parentLocalId: 1,
+				localId: 12,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "pullingFlap_leather"
+			}
+		},
+		custom: {
+			numberingLeft:
+			{
+				parentLocalId: 1,
+				localId: 13,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "custom_numberingLeft"
+			},
+			numberingRight:
+			{
+				parentLocalId: 1,
+				localId: 14,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "custom_numberingRight"
+			},
+			numberingBack:
+			{
+				parentLocalId: 1,
+				localId: 15,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "custom_numberingBack"
+			},
+			rearEngraving:
+			{
+				parentLocalId: 1,
+				localId: 16,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "custom_rearEngraving"
+			}
+		},
+		screw: {
+			screwsBaseHelmet:
+			{
+				parentLocalId: 1,
+				localId: 17,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsBase_helmet"
+			},
+			screwsBaseChinguard:
+			{
+				parentLocalId: 1,
+				localId: 18,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsBase_chinguard"
+			},
+			screwsSideNoChinguard:
+			{
+				parentLocalId: 1,
+				localId: 19,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsSide_noChinguard"
+			},
+			screwsSideChinguard:
+			{
+				parentLocalId: 1,
+				localId: 20,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsSide_chinguard"
+			},
+			screwsTop_noVisor:
+			{
+				parentLocalId: 1,
+				localId: 21,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsTop_noVisor"
+			},
+			screwsTopVisor:
+			{
+				parentLocalId: 1,
+				localId: 22,
+				matrix: [1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1],
+				SKU: "screwsTop_visor"
+			}
+		},
+		chinguard: {
+			
+		}
+	}
+]
+/*{
+		parentLocalId: 1,
+		localId: 15,
+		matrix: [1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1],
+		SKU: "custom_numberingBack"
+	},*/
+	/*
+	chinguard_groove	23
+	chinguard_noGroove	24
+	chinguard_elements	25
+	chinguardDesign_groove	26
+	chinguardDesign_noGroove	27
+	chinguardCoating_groove	28
+	chinguardCoating_noGroove	29
+	chinguardTrim_rubber	30
+	chinguardTrim_leather	31
+	visor_peak	32
+	visor_peakCoating	33
+	visor_shortShield	34
+	visor_longShield	35
+	visor_elements	36
