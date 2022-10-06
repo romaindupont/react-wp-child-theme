85e8d1b1f3551115c95d57bc05256039e72f80e9
Romain DUPONT <RDUPONT@hydequip.lan>
2022-10-06 16:18:38 +0200

    try to add specific image on order

---
+++ assets/json/elementVisor.js
@@ -0,0 +1,36 @@
+export default [{
+	"en": [
+		{
+			fileName : 'visorType',
+			title: 'visor type',
+			helpMessage: 'Choose the visor type you want'
+		},
+		{
+			fileName : 'frame',
+			title: 'frame',
+			helpMessage: 'Choose the frame to apply'
+		},
+		{
+			fileName : 'visorColor',
+			title: 'main color',
+			helpMessage: 'Choose the main color'
+		}
+	],
+	"fr": [
+		{
+			fileName : 'visorType',
+			title: 'visière',
+			helpMessage: 'Choississez le type de visière voulue'
+		},
+		{
+			fileName : 'frame',
+			title: 'frame',
+			helpMessage: 'Choississez le frame'
+		},
+		{
+			fileName : 'visorColor',
+			title: 'couleur principale',
+			helpMessage: 'Choisir la couleur principale et la finition'
+		}
+	]
+}]
--- functions.php
+++ functions.php
@@ -27,5 +27,126 @@ function get_menu() {
 	return wp_get_nav_menu_items('main-menu');
 } 
 
-add_action( 'rest_api_init', function () { register_rest_route( 'myroutes', '/menu', array( 'methods' => 'GET', 'callback' => 'get_menu', ) ); });
+add_action( 'rest_api_init', function () {
+	register_rest_route( '/imageHandler/v1', '/upload', array(
+			'methods' => 'POST',
+			'callback' => 'uploadImage',
+	) );
+} );
+
+function uploadImage($request) {
+	/* var_dump($request); */
+	$upload_dir = wp_upload_dir();
+	$upload_path = is_ssl() ? str_replace('http://', 'https://', $upload_dir['baseurl']) : $upload_dir['baseurl'];
+	$base64Image = file_get_contents('php://input');;
+	$base64Image = str_replace('data:image/png;base64,', '', $base64Image);
+	$base64Image = str_replace(' ', '+', $base64Image);
+	$base64Image = str_replace('"', '', $base64Image);
+	var_dump($base64Image);
+
+	$decoded          = base64_decode($base64Image);
+	$filename         = 'tattoo.png';
+	$hashed_filename  = md5( $filename . microtime() ) . '_' . $filename;
+	$image_upload     = file_put_contents( $upload_path . $hashed_filename, $decoded );
+	/* var_dump($image_upload); */
+}
 add_filter( 'woocommerce_store_api_disable_nonce_check', '__return_true' );
+
+function custom_new_product_image($a) {
+
+	$class = 'attachment-shop_thumbnail wp-post-image'; // Default cart thumbnail class.
+	$src = get_stylesheet_directory_uri(). '.\assets\images\imageMotorHelmet.png';
+
+	// Construct your img tag.
+	$a = '<img';
+	$a .= ' src="' . $src . '"';
+	$a .= ' class="' . $class . '"';
+	$a .= ' />';
+
+	// Output.
+	return $a;
+
+}
+
+add_filter( 'woocommerce_cart_item_thumbnail', 'custom_new_product_image' );
+
+function add_order_item_meta($item_id, $values) {
+	$key = ''; // Define your key here
+	$value = $_POST['key_name']; // Get your value here
+	woocommerce_add_order_item_meta($item_id, $key, $value);
+}
+add_action('woocommerce_add_order_item_meta', 'add_order_item_meta', 10, 2);
+
+
+// Display additional product fields (+ jQuery code)
+add_action( 'woocommerce_before_add_to_cart_button', 'njengah_additional_product_fields', 9 );
+function njengah_additional_product_fields(){
+    ?>
+    <p class="form-row validate-required" id="image" >
+        <label for="file_field"><?php echo __("Upload Image") . ': '; ?>
+            <input type='file' name='image' accept='image/*'>
+        </label>
+    </p>
+    <?php
+}
+// Add custom fields data as the cart item custom data
+add_filter( 'woocommerce_add_cart_item_data', 'add_custom_fields_data_as_custom_cart_item_data', 10, 2 );
+function add_custom_fields_data_as_custom_cart_item_data( $cart_item, $product_id ){
+    if( isset($_FILES['image']) && ! empty($_FILES['image']) ) {
+        $upload       = wp_upload_bits( $_FILES['image']['name'], null, file_get_contents( $_FILES['image']['tmp_name'] ) );
+        $filetype     = wp_check_filetype( basename( $upload['file'] ), null );
+        $upload_dir   = wp_upload_dir();
+        $upl_base_url = is_ssl() ? str_replace('http://', 'https://', $upload_dir['baseurl']) : $upload_dir['baseurl'];
+        $base_name    = basename( $upload['file'] );
+        $cart_item['file_upload'] = array(
+            'guid'      => $upl_base_url .'/'. _wp_relative_upload_path( $upload['file'] ), // Url
+            'file_type' => $filetype['type'], // File type
+            'file_name' => $base_name, // File name
+            'title'     => ucfirst( preg_replace('/\.[^.]+$/', '', $base_name ) ), // Title
+        );
+        $cart_item['unique_key'] = md5( microtime().rand() ); // Avoid merging items
+    }
+    return $cart_item;
+}
+// Display custom cart item data in cart (optional)
+add_filter( 'woocommerce_get_item_data', 'njengah_custom_item_data', 10, 2 );
+function njengah_custom_item_data( $cart_item_data, $cart_item ) {
+    if ( isset( $cart_item['file_upload']['title'] ) ){
+        $cart_item_data[] = array(
+            'name' => __( 'Image uploaded', 'woocommerce' ),
+            'value' =>  str_pad($cart_item['file_upload']['title'], 16, 'X', STR_PAD_LEFT) . '…',
+        );
+    }
+    return $cart_item_data;
+}
+// Save Image data as order item meta data
+add_action( 'woocommerce_checkout_create_order_line_item', 'njengah_field_update_order_item_meta', 20, 4 );
+function njengah_field_update_order_item_meta( $item, $cart_item_key, $values, $order ) {
+    if ( isset( $values['file_upload'] ) ){
+        $item->update_meta_data( '_img_file',  $values['file_upload'] );
+    }
+}
+// Admin orders: Display a linked button + the link of the image file
+add_action( 'woocommerce_after_order_itemmeta', 'njengah_image_link_after_order_itemmeta', 10, 3 );
+function njengah_image_link_after_order_itemmeta( $item_id, $item, $product ) {
+    // Only in backend for order line items (avoiding errors)
+    if( is_admin() && $item->is_type('line_item') && $file_data = $item->get_meta( '_img_file' ) ){
+        echo '<p><a href="'.$file_data['guid'].'" target="_blank" class="button">'.__("Open Image") . '</a></p>'; // Optional
+        echo '<p><code>'.$file_data['guid'].'</code></p>'; // Optional
+    }
+}
+// Admin new order email: Display a linked button + the link of the image file
+add_action( 'woocommerce_email_after_order_table', 'njengah_email_new_order_custom_meta_data', 10, 4);
+function njengah_email_new_order_custom_meta_data( $order, $sent_to_admin, $plain_text, $email ){
+    // On "new order" email notifications
+    if ( 'new_order' === $email->id ) {
+        foreach ($order->get_items() as $item ) {
+            if ( $file_data = $item->get_meta( '_img_file' ) ) {
+                echo '<p>
+                    <a href="'.$file_data['guid'].'" target="_blank" class="button">'.__("Download Image") . '</a><br>
+                    <pre><code style="font-size:12px; background-color:#eee; padding:5px;">'.$file_data['guid'].'</code></pre>
+                </p><br>';
+            }
+        }
+    }
+}
